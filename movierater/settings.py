ba0182db30de0bf6706c03e5d4f23dbd2f797920
landroide <landrade1313@gmail.com>
2025-03-03 18:34:41 +1300

    ENV

--- movierater/settings.py
+++ movierater/settings.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'movierater.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 REST_FRAMEWORK = {
